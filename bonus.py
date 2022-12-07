2f445c006c6f2c134cdf0458e70a37625108264a
Gurnoor <gurnoorgaby@gmail.com>
2022-12-07 21:33:27 +0530

    a

---
+++ bonus.py
@@ -0,0 +1,7 @@
+
+Salary=int(input("Current Salary:"))
+ServiceYears=int(input("Serving since:"))
+if ServiceYears>5 :
+    print ("new salary is : Rupees",Salary+1000)
+else :
+    print("no bonus")    
